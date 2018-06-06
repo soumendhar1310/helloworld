47f1c4e1ccd3ef2118d4a80d68db5ea65ab85939
soumendhar <soumendhar@9.194.173.33>
2018-06-06 18:53:53 +0800

    Test

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM java:8
+VOLUME /tmp
+ADD target/SpringBootHelloWorld-0.0.1-SNAPSHOT.jar SpringBootHelloWorld.jar
+RUN bash -c 'touch /SpringBootHelloWorld.jar'
+ENV JAVA_OPTS=""
+ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -jar /SpringBootHelloWorld.jar"]
---
+++ src/main/java/com/javainuse/controllers/TestController.java
@@ -0,0 +1,27 @@
+package com.javainuse.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/welcome")
+	/*public ModelAndView firstPage() {
+		return new ModelAndView("welcome");
+	}*/
+	public Map<String, Object> read() {
+		Map<String, Object> dataMap = new HashMap<String, Object>();
+		dataMap.put("message", "Hello World of Microservice");
+		//dataMap.put("status", "1");
+	    return dataMap;
+	}
+
+}
