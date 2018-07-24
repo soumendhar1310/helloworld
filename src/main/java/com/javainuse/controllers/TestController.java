d579559d018471883f098bb538ca6a5699906042
soumendhar1310 <soumendhar1310@gmail.com>
2018-07-24 14:07:56 +0800

    Trying from start

--- src/main/java/com/javainuse/controllers/TestController.java
+++ src/main/java/com/javainuse/controllers/TestController.java
@@ -41,7 +41,7 @@ public class TestController {
 	}*/
 	public Map<String, Object> read() {
 		Map<String, Object> dataMap = new HashMap<String, Object>();
-		dataMap.put("message", "Hello World for demo and checking for IoT demo with Sonar on VM");
+		dataMap.put("message", "Hello World for demo!!!!");
 		//dataMap.put("status", "1");
 		LOGGER.info("ELK Logback successfully worked");
 		
